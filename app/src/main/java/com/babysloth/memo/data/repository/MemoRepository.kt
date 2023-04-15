3c99e9869e6228fcab8d75de7252a08618dee391
boming <bmahn81@gmail.com>
2023-04-15 15:55:09 +0900

    MemoRepository 인터페이스 추가

--- app/src/main/java/com/babysloth/memo/data/repository/MemoRepository.kt
+++ app/src/main/java/com/babysloth/memo/data/repository/MemoRepository.kt
@@ -1,4 +1,17 @@
 package com.babysloth.memo.data.repository
 
-class MemoRepository {
+import com.babysloth.memo.data.model.Memo
+import kotlinx.coroutines.flow.Flow
+
+
+interface MemoRepository {
+    suspend fun create(memo: Memo)
+
+    suspend fun update(memo: Memo)
+
+    suspend fun delete(memo: Memo)
+
+    fun getMemos(): Flow<List<Memo>>
+    
+    fun getMemo(id: Long): Flow<Memo>
 }
