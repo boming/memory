4aaf1a6c1586eef60391365bca61500f07fdbee8
boming <bmahn81@gmail.com>
2023-04-19 20:20:32 +0900

    uistate 클래스 추가
    Co-Authored-By: Aiden Choi <aidenchoi.ys@gmail.com>

---
+++ app/src/main/java/com/babysloth/memo/ui/view/bookmark/BookmarkUiState.kt
@@ -0,0 +1,8 @@
+package com.babysloth.memo.ui.view.bookmark
+
+import com.babysloth.memo.domain.model.Memo
+import kotlinx.coroutines.flow.Flow
+
+data class BookmarkUiState(
+    val memos: List<Memo>? = null
+)
