baea4665bf35ee297244747211beb6f08e9ad788
boming <bmahn81@gmail.com>
2023-04-25 17:02:42 +0900

    hilt database module 추가

--- app/src/main/java/com/babysloth/memo/di/DatabaseModule.kt
+++ app/src/main/java/com/babysloth/memo/di/DatabaseModule.kt
@@ -1,4 +1,20 @@
 package com.babysloth.memo.di
 
+import android.content.Context
+import com.babysloth.memo.data.database.MemoRoomDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
 class DatabaseModule {
+    @Provides
+    fun provideMemoDatabase(
+        @ApplicationContext context: Context
+    ): MemoRoomDatabase {
+        return MemoRoomDatabase.getDatabase(context)
+    }
 }
